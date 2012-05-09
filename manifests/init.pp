23121609fb35adc9ce3a1481ff6ec2885f641e94
Thomas Sutton <me@thomas-sutton.id.au>
2012-05-09 17:54:58 +0800

    Move nginx-centric code into nginx module
    This should make the nginx branch and master branch compatible. Which is to say
    
        include drupal
    
    is a no-op.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,33 +1,3 @@
-#
-# = Class: drupal
-#
-# Install and configure services to host Drupal sites.
-#
-# == Requires:
-#
-# thsutton-nginx
-# thsutton-mysql
-# thsutton-php
-#
-# == Sample Usage:
-#
-#   include drupal
-#
 class drupal {
-
-  include nginx
-  include php::sapi::fpm
-  include mysql::server
-
-  php::extension { 'apc' : ensure => enabled }
-  php::extension { 'gd' : ensure => enabled }
-  php::extension { 'mysql' : ensure => enabled }
-  php::extension { 'curl' : ensure => enabled }
-
-  # php::fpm::pool { 'www' : ensure => absent }
-  # php::fpm::pool { 'drupal': ensure => present }
-
-  include drupal::drush
-  include drupal::configuration
 
 }
--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,8 +1,32 @@
 #
 # = Class: drupal::nginx
 #
-# Install and configure the Nginx server.
+# Install and configure services to host Drupal sites.
 #
+# == Requires:
+#
+# thsutton-nginx
+# thsutton-mysql
+# thsutton-php
+#
+# == Sample Usage:
+#
+#   include drupal::nginx
 #
 class drupal::nginx {
+
+  include nginx
+  include php::sapi::fpm
+  include mysql::server
+
+  php::extension { 'apc' : ensure => enabled }
+  php::extension { 'gd' : ensure => enabled }
+  php::extension { 'mysql' : ensure => enabled }
+  php::extension { 'curl' : ensure => enabled }
+
+  # php::fpm::pool { 'www' : ensure => absent }
+  # php::fpm::pool { 'drupal': ensure => present }
+
+  include drupal::drush
+  include drupal::configuration
 }
